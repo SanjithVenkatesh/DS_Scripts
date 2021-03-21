87130ae960b7bab0c223bd134d194a77cc6cade1
Sanjith Venkatesh <sanjith@pacbell.net>
2021-03-21 14:48:55 -0700

    Anitamtion version

--- pi.py
+++ pi.py
@@ -1,101 +1,105 @@
 """
 pi.py by Sanjith Venkatesh
-Based on video by Jorma Tech: https://www.youtube.com/watch?v=pvimAM_SLic&t=421s
+Based on video by Joma Tech: https://www.youtube.com/watch?v=pvimAM_SLic&t=421s
 Script approximates pi and shows visualizations to show the dots and circle
 Usage:
-  pi.py <radius> <dots> <rounds>
+  pi.py <radius>
   pi.py -h | --help
 """
 import math
 import random
-import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from functools import reduce
 from docopt import docopt
+from matplotlib.animation import FuncAnimation
+
+plt.style.use('seaborn-whitegrid')
+fig, ax = plt.subplots(figsize=(10,10))
+fig.set_tight_layout(True)
+XYs = []
+PIs = []
+radius = 0
+dots = 0
 
 
 class XY:
-    def __init__(self,x,y):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
 
     def to_zero(self):
         return math.sqrt(self.x**2 + self.y**2)
 
-    def in_circle(self, radius):
+    def in_circle(self):
         return True if self.to_zero() < radius else False
+
+    def printXY(self):
+        print(self.x, self.y)
+
 
 def average(lst):
     return reduce(lambda a, b: a + b, lst) / len(lst)
 
-def arc_coord(radius):
-    P1 = XY(0,1)
-    theta = np.linspace(0,90,9000)
-    x = []
-    y = []
-    for angle in theta:
-        x.append(P1.x + radius*math.sin(angle))
-        y.append(P1.y - radius*(1-math.cos(angle)))
-    return {"x": x, "y": y}
+
+def arc_coord():
+    theta = np.linspace(0, 2*np.pi, 100)
+
+    r = radius
+
+    x1 = r*np.cos(theta)
+    x2 = r*np.sin(theta)
+    return [x1, x2]
 
 
-def plot_simulation(radius, XYs, pis):
-    num_cols = 2
-    num_rows = math.ceil(len(XYs)/num_cols)
-    fig, axs = plt.subplots(num_rows, num_cols, figsize=(10,8))
+def update(i):
+    global fig
+    global ax
 
-    fig.suptitle("Overall Pi value = " + str(average(pis)))
-    arc_coords = arc_coord(radius)
-    spot = 0
-    for row in range(0,num_rows):
-        for col in range(0,num_cols):
-            if spot < len(XYs):
-                axs[row,col].plot(arc_coords["x"], arc_coords["y"], color="blue", linewidth=5)
+    plt.cla()
 
-                for XY in XYs[spot]:
-                    if XY.in_circle(radius):
-                        axs[row,col].scatter(XY.x, XY.y, color="green")
-                    else:
-                        axs[row,col].scatter(XY.x, XY.y, color="red")
-                
-                axs[row,col].set_ylim(0,1)
-                axs[row,col].set_xlim(0,1)
-                axs[row,col].set_title("Approximate Value of Pi = " + str(pis[spot]))
-                spot += 1
-    plt.show()
+    XYs_to_use = gen_rand_coord()
+    pi = approximate_pi(XYs_to_use)
 
-def rand_coords(radius, dots, rounds):
-    XY_rounds = list()
+    fig.suptitle("Overall Pi value = " + str(pi))
+    arc_coords = arc_coord()
+    ax.plot(arc_coords[0], arc_coords[1], color="blue", linewidth=1)
+    for XY in XYs_to_use:
+        if XY.in_circle():
+            ax.scatter(XY.x, XY.y, color="green")
+        else:
+            ax.scatter(XY.x, XY.y, color="red")
+    ax.set_xlim(radius*-1 - (radius*0.1), radius + (radius * 0.1))
+    ax.set_ylim(radius*-1 - (radius * 0.1), radius + (radius * 0.1))
 
-    for _ in range(0,rounds):
-        rand_XY = list()
-        
-        for _ in range(0,dots):
-            x = random.uniform(0,radius)
-            y = random.uniform(0,radius)
-            rand_XY.append(XY(x,y))
-        XY_rounds.append(rand_XY)
-    
-    return XY_rounds
 
-def approximate_pi(XYs, radius):
-    PIs = list()
-    for xy in XYs:
-        in_circle = [c for c in xy if c.in_circle(radius)]
-        in_circle_count = len(in_circle)
+def gen_rand_coord():
+    global XYs
 
-        PIs.append(4* (in_circle_count/len(xy)))
-    return PIs
+    x = random.uniform(radius*-1, radius)
+    y = random.uniform(radius*-1, radius)
+
+    XYs.append(XY(x, y))
+    return XYs
+
+
+def approximate_pi(XYs):
+    global PIs
+    in_circle = [c for c in XYs if c.in_circle()]
+    in_circle_count = len(in_circle)
+
+    return (4 * (in_circle_count/len(XYs)))
+
+
+def prepare_vars(r):
+    global radius
+    radius = r
 
 
 if __name__ == "__main__":
     args = docopt(__doc__)
 
-    radius = int(args["<radius>"]) if args["<radius>"] is not None else 1
-    dots = int(args["<dots>"]) if args["<dots>"] is not None else 100
-    rounds = int(args["<rounds>"]) if args["<rounds>"] is not None else 4
-
-    XYs = rand_coords(radius, dots, rounds)
-    PIs = approximate_pi(XYs, radius)
-    plot_simulation(radius, XYs, PIs)
+    r = int(args["<radius>"]) if args["<radius>"] is not None else 1
+    prepare_vars(r)
+    anim = FuncAnimation(fig, update, interval=500)
+    plt.show()
