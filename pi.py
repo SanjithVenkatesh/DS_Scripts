2769618a1599c1cbdf030d904167559a5be24da8
Sanjith Venkatesh <sanjith@pacbell.net>
2021-03-22 00:04:22 -0700

    New figure to show convergence of pi

--- pi.py
+++ pi.py
@@ -15,8 +15,11 @@ from docopt import docopt
 from matplotlib.animation import FuncAnimation
 
 plt.style.use('seaborn-whitegrid')
-fig, ax = plt.subplots(figsize=(10,10))
-fig.set_tight_layout(True)
+fig1, ax1 = plt.subplots(figsize=(10,10))
+fig1.set_tight_layout(True)
+fig2, ax2 = plt.subplots(figsize=(12,8))
+fig2.set_tight_layout(True)
+rounds = 0
 XYs = []
 PIs = []
 radius = 0
@@ -53,24 +56,31 @@ def arc_coord():
 
 
 def update(i):
-    global fig
-    global ax
+    global fig1
+    global ax1
+    global fig2
+    global ax2
+    global rounds
 
     plt.cla()
 
     XYs_to_use = gen_rand_coord()
     pi = approximate_pi(XYs_to_use)
+    rounds += 1
 
-    fig.suptitle("Overall Pi value = " + str(pi))
+    fig1.suptitle("Overall Pi value = " + str(pi))
+    fig2.suptitle("Change in Overall Value of Pi")
     arc_coords = arc_coord()
-    ax.plot(arc_coords[0], arc_coords[1], color="blue", linewidth=1)
+    rounds_x = np.arange(0,rounds,1)
+    ax1.plot(arc_coords[0], arc_coords[1], color="blue", linewidth=1)
+    ax2.plot(rounds_x, PIs, color="blue", linewidth=1)
     for XY in XYs_to_use:
         if XY.in_circle():
-            ax.scatter(XY.x, XY.y, color="green")
+            ax1.scatter(XY.x, XY.y, color="green")
         else:
-            ax.scatter(XY.x, XY.y, color="red")
-    ax.set_xlim(radius*-1 - (radius*0.1), radius + (radius * 0.1))
-    ax.set_ylim(radius*-1 - (radius * 0.1), radius + (radius * 0.1))
+            ax1.scatter(XY.x, XY.y, color="red")
+    ax1.set_xlim(radius*-1 - (radius*0.1), radius + (radius * 0.1))
+    ax1.set_ylim(radius*-1 - (radius * 0.1), radius + (radius * 0.1))
 
 
 def gen_rand_coord():
@@ -88,7 +98,9 @@ def approximate_pi(XYs):
     in_circle = [c for c in XYs if c.in_circle()]
     in_circle_count = len(in_circle)
 
-    return (4 * (in_circle_count/len(XYs)))
+    new_pi = (4 * (in_circle_count/len(XYs)))
+    PIs.append(new_pi)
+    return new_pi
 
 
 def prepare_vars(r):
@@ -101,5 +113,6 @@ if __name__ == "__main__":
 
     r = int(args["<radius>"]) if args["<radius>"] is not None else 1
     prepare_vars(r)
-    anim = FuncAnimation(fig, update, interval=500)
+    anim = FuncAnimation(fig1, update, interval=500)
+    anim2 = FuncAnimation(fig2, update, interval=500)
     plt.show()
